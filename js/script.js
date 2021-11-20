ed96ddc0f941505cff6f9bc06bb74e00dcb89878
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2021-11-20 16:10:05 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,82 @@
+// Lógica
+// 1 - Selecionar elementos que devem ser animados
+// 2 - Definir a classe que é adicionada durante a animação
+// 3 - Criar função de animação
+// 3.1 - Verificar a distância entre a barra de scroll e o topo do site
+// 3.2 - Verificar se a distância do 3.1 + Offset é maior do que a distância entre o elemento e o Topo da Página.
+// 3.3 - Se verdadeiro adicionar classe de animação, remover se for falso.
+// 4 - Ativar a função de animação toda vez que o usuário utilizar o Scroll
+// 5 - Otimizar ativação
+
+
+//?     https://www.youtube.com/watch?v=pKTOT63X9XQ
+
+
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//*  | projeto no modo performatico | com biblioteca LODASH
+//*  https://lodash.com/
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+const debounce = function(func, wait, immediate) {
+   let timeout;
+   return function(...args) {
+     const context = this;
+     const later = function () {
+       timeout = null;
+       if (!immediate) func.apply(context, args);
+     };
+     const callNow = immediate && !timeout;
+     clearTimeout(timeout);
+     timeout = setTimeout(later, wait);
+     if (callNow) func.apply(context, args);
+   };
+ };
+ 
+ const target = document.querySelectorAll('[data-anime]');
+ const animationClass = 'animate';
+ 
+ function animeScroll() {
+   const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+   target.forEach(function(element) {
+     if((windowTop) > element.offsetTop) {
+       element.classList.add(animationClass);
+     } else {
+       element.classList.remove(animationClass);
+     }
+   })
+ }
+ 
+ animeScroll();
+ 
+ if(target.length) {
+   window.addEventListener('scroll', debounce(function() {
+     animeScroll();
+   }, 100));
+ }
+
+//! %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//!  start | aqui já é todo o projeto porem não está performático |
+//! %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// const target = document.querySelectorAll('[data-anime]')
+// const animationClass = 'animate'
+
+// function animeScrool() {
+//    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4)
+//       target.forEach(function (element) {
+//          if ((windowTop) > element.offsetTop) {
+//             element.classList.add(animationClass)
+//          } else {
+//             element.classList.remove(animationClass)
+//          }
+//       })
+// }
+
+// animeScrool()
+
+// if(target.length) {
+//    window.addEventListener('scroll', function () {
+//       animeScrool()
+//    })
+// }
+//! %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//!  end | aqui já é todo o projeto porem não está performático |
+//! %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
